abcd37f69f9c98edb4b0a8728b543eeaf4b79a0d
Jan Hendrik van Petegem <jan.hendrik.van.petegem@swov.nl>
2017-10-09 08:46:41 +0200

    improvement plot4 R code

--- plot4.R
+++ plot4.R
@@ -10,21 +10,24 @@ dfplot <- read.table("household_power_consumption_subset.txt", #nrows = 10,
         mutate(DateTime = dmy_hms(paste(Date, Time, sep = " ")),
                Weekday = wday(DateTime, label=TRUE))
 
+# set device
+png(filename = "plot4.png", width = 480, height=480 )
+
 #set plots per device
 par(mfcol=c(2,2))
 
 #plot1
-plot(x=dfplot$DateTime, y = dfplot$Global_active_power, type="n", xlab = "datetime", 
+plot(x=dfplot$DateTime, y = dfplot$Global_active_power, type="n", xlab = "", 
      ylab = "Global Active Power (kilowatts)")
 lines(x=dfplot$DateTime, y = dfplot$Global_active_power)
 
 #plot2
-plot(x=dfplot$DateTime, y= dfplot$Sub_metering_1, type="n", xlab = "datetime", 
+plot(x=dfplot$DateTime, y= dfplot$Sub_metering_1, type="n", xlab = "", 
      ylab = "Energy sub metering")
 lines(x=dfplot$DateTime, y = dfplot$Sub_metering_1)
 lines(x=dfplot$DateTime, y = dfplot$Sub_metering_2, col="red")
 lines(x=dfplot$DateTime, y = dfplot$Sub_metering_3, col="blue")
-legend("topright", lty=1, col = c("black", "red", "blue"), 
+legend("topright", lty=1, col = c("black", "red", "blue"), bty="n",
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 #plot3
@@ -37,5 +40,5 @@ plot(x=dfplot$DateTime, y = dfplot$Global_reactive_power , type="n", xlab = "dat
      ylab = "Global reactive Power (kilowatts)")
 lines(x=dfplot$DateTime, y = dfplot$Global_reactive_power)
 
-dev.copy(png, width = 480, height=480, file = "plot4.png")
+#dev.copy(png, width = 480, height=480, file = "plot4.png")
 dev.off()
